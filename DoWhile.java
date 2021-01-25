f397c2cbbf02c4bf60ce6d065105faaa8ed459de
Anshuman Singh <anshumaansingh98@gmail.com>
2021-01-25 22:25:31 +0530

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile {
+    public static void main(String ar[])
+    {
+        int i=1, a = 10;
+        do
+        {
+            System.out.println(i+" Hello");
+            i++;
+        }
+        while(i<=a);
+        System.out.println("Loop has been terminated");
+    }
+    
+}
