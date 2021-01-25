3dde4fc095a2171673d574774bd256a208b8e90b
Anshuman Singh <anshumaansingh98@gmail.com>
2021-01-25 22:19:30 +0530

    Create While.java

---
+++ While.java
@@ -0,0 +1,14 @@
+public class While 
+{
+    public static void main(String ar[])
+    {
+        int i=1, a=10;
+        while(i<=a)
+        {
+            System.out.println(i+" Hello");
+            i++;
+        }
+        System.out.println("Loop has been terminated");
+    }
+    
+}
